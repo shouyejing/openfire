ee621620e4f93b86cd38c2c377ba4ed0a86133f8
Gwenaël D'AUBENTON <gd@openfire.fr>
2015-07-15 17:19:21 +0200

    Ajout module migre vers version 8 of_zz_sarment (archives seulement)

---
+++ of_zz_sarment/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+        "name" : "OpenFire / Module Le Sarment",
+        "version" : "1.1",
+        "author" : "OpenFire",
+        "website" : "http://www.openfire.fr",
+        "category" : "Generic Modules/Others",
+        "description": """ Module spécifique pour Le Sarment
+- Ajouter les archives
+""",
+        "depends" : ["of_base"],
+        "init_xml" : [ ],
+        "demo_xml" : [ ],
+        "update_xml" : [
+#            'security/ir.model.access.csv',
+            'of_zz_sarment_view.xml'
+        ],
+        "installable": True,
+        'active': False,
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
